0cc6c2855be6fbf025cab88d5e7e841d02f3730c
Stanislav <staniskudriavtsev@yandex.ru>
2022-02-01 22:14:57 +0300

    stdlib: standard miscellaneous

---
+++ libftx/stdlib/stdlib.h
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   stdlib.h                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: stanislav <student.21-school.ru>           +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/11/03 22:29:20 by stanislav         #+#    #+#             */
+/*   Updated: 2022/02/01 22:10:05 by stanislav        ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef  STDLIB_H
+# define STDLIB_H
+
+# include "stddef.h"
+
+int		ft_atoi(const char *nptr);
+
+void	*ft_calloc(size_t nmemb, size_t size);
+
+char	*ft_itoa(int n);
+
+#endif
