dc8fbc9a17e2979b7ca39605fecc534d516cd100
Stanislav <staniskudriavtsev@yandex.ru>
2022-02-01 20:29:50 +0300

    ctype: char funcs

--- libftx/ctype/ctype.h
+++ libftx/ctype/ctype.h
@@ -3,28 +3,28 @@
 /*                                                        :::      ::::::::   */
 /*   ctype.h                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: stanislav <stanislav@student.42.fr>        +#+  +:+       +#+        */
+/*   By: stanislav <student.21-school.ru>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2021/10/18 22:00:54 by stanislav         #+#    #+#             */
-/*   Updated: 2021/10/18 22:00:55 by stanislav        ###   ########.fr       */
+/*   Created: 2022/01/20 23:40:54 by stanislav         #+#    #+#             */
+/*   Updated: 2022/01/20 23:40:54 by stanislav        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef  CTYPE_H
+#ifndef CTYPE_H
 # define CTYPE_H
 
-int		ft_isalnum(int c);
-int		ft_isalpha(int c);
-int		ft_isascii(int c);
-int		ft_isblank(int c);
-int		ft_iscntrl(int c);
-int		ft_isdigit(int c);
-int		ft_isgraph(int c);
-int		ft_islower(int c);
-int		ft_isprint(int c);
-int		ft_ispunct(int c);
-int		ft_isspace(int c);
-int		ft_isupper(int c);
-int		ft_isxdigit(int c);
+int	ft_isalnum(int c);
+int	ft_isalpha(int c);
+int	ft_isascii(int c);
+int	ft_isblank(int c);
+int	ft_iscntrl(int c);
+int	ft_isdigit(int c);
+int	ft_isgraph(int c);
+int	ft_islower(int c);
+int	ft_isprint(int c);
+int	ft_ispunct(int c);
+int	ft_isspace(int c);
+int	ft_isupper(int c);
+int	ft_isxdigit(int c);
 
 #endif
