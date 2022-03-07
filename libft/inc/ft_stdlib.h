ab5f9db396195e81dcf42b76d065e4df96691634
Stanislav <staniskudriavtsev@yandex.ru>
2022-03-07 17:34:51 +0300

    stdlib: atoi_base

--- libft/inc/ft_stdlib.h
+++ libft/inc/ft_stdlib.h
@@ -6,16 +6,16 @@
 /*   By: stanislav <student.21-school.ru>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/21 22:40:56 by stanislav         #+#    #+#             */
-/*   Updated: 2022/02/23 18:27:36 by stanislav        ###   ########.fr       */
+/*   Updated: 2022/03/07 17:31:16 by stanislav        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef FT_STDLIB_H
 # define FT_STDLIB_H
 
-# include <stddef.h> 
 # include <stdlib.h>
 
+int		ft_atoi_base(const char *nptr, const char *base);
 int		ft_atoi(const char *nptr);
 
 void	*ft_calloc(size_t nmemb, size_t size);
--- libft/src/stdlib/ft_atoi.c
+++ libft/src/stdlib/ft_atoi.c
@@ -3,61 +3,17 @@
 /*                                                        :::      ::::::::   */
 /*   ft_atoi.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: mhorton <mhorton@student.42.fr>            +#+  +:+       +#+        */
+/*   By: stanislav <student.21-school.ru>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/02/20 12:47:45 by mhorton           #+#    #+#             */
-/*   Updated: 2022/02/20 12:47:45 by mhorton          ###   ########.fr       */
+/*   Created: 2022/03/07 16:07:36 by stanislav         #+#    #+#             */
+/*   Updated: 2022/03/07 16:07:37 by stanislav        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <limits.h>
-#include "ft_ctype.h"
-
-static void	ft_skip(const char **nptr, char *sign)
-{
-	while (ft_isspace(**nptr))
-		(*nptr)++;
-	if (**nptr == '-' || **nptr == '+')
-	{
-		if (**nptr == '-')
-			*sign = -1;
-		(*nptr)++;
-	}
-	while (**nptr == '0')
-		(*nptr)++;
-}
-
-// nbr is always a positive number while in the loop of ft_atoi
-static int	ft_isoverflow(long nbr, unsigned char digit, char sign)
-{
-	if (sign < 0)
-		return ((LONG_MIN / 10 > nbr) || \
-				(LONG_MIN + digit > -10 * nbr));
-	if (sign > 0)
-		return ((LONG_MAX / 10 < nbr) || \
-				(LONG_MAX - digit < 10 * nbr));
-	return (-1);
-}
+#include "ft_stdlib.h"
 
 // libft-unit-test - that is why such an implementation
 int	ft_atoi(const char *nptr)
 {
-	long	nbr;
-	char	sign;
-
-	sign = 1;
-	nbr = 0;
-	ft_skip(&nptr, &sign);
-	while (ft_isdigit(*nptr))
-	{
-		if (ft_isoverflow(nbr, *nptr - '0', sign))
-		{
-			if (sign == 1)
-				return (-1);
-			return (0);
-		}
-		nbr = 10 * nbr + (*nptr - '0');
-		nptr++;
-	}
-	return (sign * nbr);
+	return (ft_atoi_base(nptr, "0123456789"));
 }
