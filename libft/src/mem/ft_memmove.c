4a1a9b9a91107ca9455c5134fc469bb324eda7a2
Stanislav D. Kudriavtsev <staniskudriavtsev@yandex.ru>
2022-05-03 12:54:54 +0300

    ft_memmove improved

--- libft/src/mem/ft_memmove.c
+++ libft/src/mem/ft_memmove.c
@@ -6,7 +6,7 @@
 /*   By: stanislav <student.21-school.ru>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/23 19:48:35 by stanislav         #+#    #+#             */
-/*   Updated: 2022/02/23 19:48:35 by stanislav        ###   ########.fr       */
+/*   Updated: 2022/05/03 12:52:41 by stanislav        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,10 +21,10 @@ void	*ft_memmove(void *dst, const void *src, size_t n)
 		return (dst);
 	dest = dst;
 	source = src;
-	if (src < dst)
+	if (src < dst && dst - src < (ptrdiff_t) n)
 		while (n--)
 			dest[n] = source[n];
-	if (src > dst)
+	else
 		ft_memcpy(dst, src, n);
 	return (dst);
 }
